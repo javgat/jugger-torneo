738e2d22071034a1493f9bb349da12148d73341a
Javier Gatón Herguedas <javigaton@gmail.com>
2021-08-31 12:59:27 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'jugger-torneo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
