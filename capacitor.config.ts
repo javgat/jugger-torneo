f5ffc7cd5950d294ef79e7a55f980b93410bee76
Javier Gatón Herguedas <javigaton@gmail.com>
2021-09-16 09:50:34 +0200

    added icons and splash screen, script for android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.javgat.jugger-torneo',
   appName: 'jugger-torneo',
   webDir: 'www',
   bundledWebRuntime: false
